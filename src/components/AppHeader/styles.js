c9d7e93f9de3ad3de2f62676d1745b88d57a38e8
4lex-passos <alex.yomare@gmail.com>
2021-02-23 12:11:29 -0300

    update style

--- src/components/AppHeader/styles.js
+++ src/components/AppHeader/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,400;0,600;0,800;0,900;1,200&display=swap";
 export const Container = styled.div`
     display: flex;
     gap: 20px;
@@ -18,7 +18,7 @@ export const Title = styled.h1`
     font-weight: bolder;
     font-size: 30px;
     margin-left: 5px;
-    font-family: "Poppins", cursive;
+    font-family: 'Poppins', sans-serif;
     color: #532b69;
     line-height: 25px;
 
