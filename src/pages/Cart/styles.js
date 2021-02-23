26e287841223f813a57306bf8cc233815f54bc29
4lex-passos <alex.yomare@gmail.com>
2021-02-23 12:57:56 -0300

    Update styles.js

--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -11,7 +11,7 @@ export const Container = styled.div`
 
     @media (max-width: 1000px) {
         width: 100vw;
-        height: 100%;
+        height: 100vh;
         border-radius: 0px;
         padding: 20px;
     }
