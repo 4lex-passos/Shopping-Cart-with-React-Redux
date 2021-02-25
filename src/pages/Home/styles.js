9a20ce2b37c8d15f0b2897b505c6c1f192ad8004
4lex-passos <alex.yomare@gmail.com>
2021-02-25 20:33:01 -0300

    Update styles.js

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -7,6 +7,7 @@ export const Container = styled.div`
     padding: 20px;
     background-color: #fff;
     border-radius: 5px;
+    box-shadow: 10px 10px 8px rgba(0,0,0,.5);
 
     @media (max-width: 1000px) {
         min-height: 100vh;
